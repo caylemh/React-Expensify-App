7f64146a4d58d3488af5b0fe649d718176770dbb
Caylem <caylemh@gmail.com>
2020-04-13 21:51:27 +0200

    ES6 Array Destructuring

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,7 @@
+//
+//Object Destructuring
+//
+
 // const person = {
 //     name:'Caylem',
 //     age: 36,
@@ -15,13 +19,25 @@
 //     console.log(`It's ${temperature} in ${city}.`);
 // }
 
-const book ={
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book ={
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const { name: publisherName = 'Self-Published' } = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+// console.log(publisherName);
+
+//
+// Array Destructuring
+//
+
+const address = ['1299 S Junioer Street', 'Johannesburg', 'Gauteng', '2001'];
+const [, city, province = 'Limpopo'] = address;
+console.log(`You are in ${city}, ${province}.`);
+
+const item = ['Hot Coffee', 'R19,00', 'R21,00', '25,00'];
+const [name, , medium] = item;
+console.log(`A Medium ${name} costs ${medium}.`)
